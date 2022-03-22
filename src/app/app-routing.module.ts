245afa4241838a3c444584add9132e9e86acd396
mcflav <nc2jax@hotmail.com>
2022-03-21 23:17:22 -0400

    optimized code.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { OrdersComponent } from './orders/orders.component';
-import { EditOrderComponent } from './orders/edit-order/edit-order.component';
+import { AboutComponent } from './about/about.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'register', component: RegisterComponent},
-    { path: 'login', component: LoginComponent},
-    { path: 'orders/:email/:firstname/:lastname/:id', component: OrdersComponent},
+    { path: 'about', component: AboutComponent },
+    { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+    { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule)},
+    { path: 'orders/:email/:firstname/:lastname/:id', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)},
     // children: [
     //     //use path below if you use resolver service file to get data dynamically.
     //     // { path: ':id', component: OrderComponent, resolve: {order: OrderResolver} },
-    //     { path: ':id/edit', component: EditOrderComponent}
+    //     { path: ':id/edit', loadChildren: () => import('./orders/edit-order/edit-order.module').then(m => m.EditOrderModule)}
     // ]},
     {path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
     {path: '**', redirectTo: '/not-found'}
@@ -24,7 +22,7 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
     ],
     exports: [RouterModule]
 })
